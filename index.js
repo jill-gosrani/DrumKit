d45c8ff08b9553439227366e539680c7b4876753
jill-gosrani <jillgosrani15@gmail.com>
2024-02-18 23:37:02 -0500

    added eventlistener for keydown

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 var listt = document.querySelectorAll(".drum");
-// var audio = new Audio('./sounds/tom-1.mp3');
 for(var i =0; i < listt.length;i++){
-    listt[i].addEventListener("click", function (){
-        switch (this.innerHTML) {
-            case 'w':
-                new Audio('./sounds/tom-1.mp3').play();
-                break;
-            case 'a':
-                new Audio('./sounds/tom-2.mp3').play();
-                break;
-            case 's':
-                new Audio('./sounds/tom-3.mp3').play();
-                break;
-            case 'd':
-                new Audio('./sounds/tom-4.mp3').play();
-                break;
-            case 'j':
-                new Audio('./sounds/snare.mp3').play();
-                break;
-            case 'k':
-                new Audio('./sounds/crash.mp3').play();
-                break;                      
-            default:
-                new Audio('./sounds/kick-bass.mp3').play();
-        }
+    listt[i].addEventListener("click", function (event){
+        checkKey(this.innerHTML);
     });
 }
-
-// audio.play();
+document.addEventListener("keydown", function(event){
+    checkKey(event.key);
+});
+function checkKey(key){
+    switch (key) {
+        case 'w':
+            new Audio('./sounds/tom-1.mp3').play();
+            break;
+        case 'a':
+            new Audio('./sounds/tom-2.mp3').play();
+            break;
+        case 's':
+            new Audio('./sounds/tom-3.mp3').play();
+            break;
+        case 'd':
+            new Audio('./sounds/tom-4.mp3').play();
+            break;
+        case 'j':
+            new Audio('./sounds/snare.mp3').play();
+            break;
+        case 'k':
+            new Audio('./sounds/crash.mp3').play();
+            break;                      
+        default:
+            new Audio('./sounds/kick-bass.mp3').play();
+    }
+}
